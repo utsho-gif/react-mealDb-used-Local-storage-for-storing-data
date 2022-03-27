6b20f6365215085ab279bf034732d229fedb0b06
Iftekhar Utsho <future.slangsta76@gmail.com>
2022-03-28 02:05:44 +0600

    added localstorage for data store

---
+++ src/components/OrderList/OrderList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const OrderList = (props) => {
+    const { orders } = props;
+
+    
+    let count = 0;
+    for(const meal of orders){
+        count = count + meal.quantity;
+    }
+    
+
+    
+    return (
+        <div>
+            <h2>Order List</h2>
+            <h4>Items Ordered: {count}</h4>
+        </div>
+    );
+};
+
+export default OrderList;
